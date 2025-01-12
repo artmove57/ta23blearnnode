ea39ce24c959f88f43b60ada5aaa648a5d690e1c
Artur Genno <artur.genno@tptlive.ee>
2025-01-12 20:49:07 +0200

    Add scss-loader

--- webpack.config.js
+++ webpack.config.js
@@ -23,11 +23,15 @@ export default {
             test: /\.css$/i,
             use: ["style-loader","css-loader"],
           },
+          {
+            test: /\.scss$/i,
+            use: ["style-loader", "css-loader", "sass-loader"],
+          },
         ],
       },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html'
+            template: "./src/index.html"
         }),
     ],
 };
