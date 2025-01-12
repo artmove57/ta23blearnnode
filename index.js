332f9d2e65efdbfefc3c6690c831a1a42d846604
Artur Genno <artur.genno@tptlive.ee>
2025-01-12 20:07:56 +0200

    Add css-loader and style-loader

--- index.js
+++
@@ -1,9 +0,0 @@
-import common from './common.js';
-common.hello();
-import es from './es.js';
-es.hello();
-
-//const common = require('./common.js');
-//common.hello();
-//const es = require('./es.js').default;
-//es.hello();
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+const __filename = import.meta.filename;
+const __dirname =import.meta.dirname
+
+export default {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+      },
+      module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ["style-loader","css-loader"],
+          },
+        ],
+      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+    ],
+};
